088beccb2850b81b9e9b293c97ee4c39c6228d3f
hebaashraf21 <heba.elmaaboud00@eng-st.cu.edu.eg>
2023-05-18 20:34:30 +0300

    decrease battery bar when collision with building

--- source/states/play-state.hpp
+++ source/states/play-state.hpp
@@ -7,142 +7,93 @@
 #include <systems/free-camera-controller.hpp>
 #include <systems/movement.hpp>
 #include <asset-loader.hpp>
-
+#include "../common/ecs/game-controller.hpp"
 // This state shows how to use the ECS framework and deserialization.
-class Playstate: public our::State {
+class Playstate : public our::State
+{
 
     our::World world;
     our::ForwardRenderer renderer;
     our::FreeCameraControllerSystem cameraController;
     our::MovementSystem movementSystem;
-    our::CollisionSystem collisionSystem;    
+    our::CollisionSystem collisionSystem;
+    our::GameController gameController;
 
-    int numberOfBatteries=5;
-    int counterToRemove=0;
-    int tempCount=0;
+    int numberOfBatteries = 5;
+    int counterToRemove = 0;
+    int tempCount = 0;
+    int packagesNumber = 0;
 
-    
-    void increaseBatteries(){
-        if(numberOfBatteries==4){
-            numberOfBatteries=5;
-
-            world.markAsUnRemoval("e5");
-            cameraController.changeSpeed(0.7f);
-        }
-        else if(numberOfBatteries==3)
-        {
-            numberOfBatteries=4;
-            world.markAsUnRemoval("e4");
-            cameraController.changeSpeed(0.6f);
-        }
-        else if(numberOfBatteries==2)
-        {
-            numberOfBatteries=3;
-            world.markAsUnRemoval("e3");
-            cameraController.changeSpeed(0.5f);
-        }
-        else if(numberOfBatteries==1)
-        {
-            numberOfBatteries=2;
-            world.markAsUnRemoval("e3");
-            cameraController.changeSpeed(0.4f);
-        }
-    }
-
-    void decreaseBatteries(){
-        if(counterToRemove==-180){
-            counterToRemove=0;
-            if(numberOfBatteries==5){
-                numberOfBatteries=4;
-                world.removeBatteryEntity("e5");
-                cameraController.changeSpeed(0.6f);
-            }
-            else if(numberOfBatteries==4)
-            {
-                numberOfBatteries=3;
-                world.removeBatteryEntity("e4");
-                cameraController.changeSpeed(0.5f);
-            }
-            else if(numberOfBatteries==3)
-            {
-                numberOfBatteries=2;
-                world.removeBatteryEntity("e3");
-                cameraController.changeSpeed(0.4f);
-            }
-            else if(numberOfBatteries==2)
-            {
-                numberOfBatteries=1;
-                world.removeBatteryEntity("e2");
-                cameraController.changeSpeed(0.3f);
-            }
-            else
-            {
-                numberOfBatteries=0;
-                world.removeBatteryEntity("e1");
-                getApp()->changeState("gameover");
-                /// Game over
-            }
-        }
-    }
-
-    void onInitialize() override {
-        numberOfBatteries=5;
-        counterToRemove=0;
-        tempCount=0;
-
+    void onInitialize() override
+    {
+        numberOfBatteries = 5;
+        counterToRemove = 0;
+        tempCount = 0;
+        gameController.enter(getApp(), &world);
         // First of all, we get the scene configuration from the app config
-        auto& config = getApp()->getConfig()["scene"];
+        auto &config = getApp()->getConfig()["scene"];
         // If we have assets in the scene config, we deserialize them
-        if(config.contains("assets")){
+        if (config.contains("assets"))
+        {
             our::deserializeAllAssets(config["assets"]);
         }
         // If we have a world in the scene config, we use it to populate our world
-        if(config.contains("world")){
+        if (config.contains("world"))
+        {
             world.deserialize(config["world"]);
         }
         // We initialize the camera controller system since it needs a pointer to the app
-        cameraController.enter(getApp());
+        cameraController.enter(getApp(), &gameController);
         // Then we initialize the renderer
         auto size = getApp()->getFrameBufferSize();
         renderer.initialize(size, config["renderer"]);
     }
 
-    void onDraw(double deltaTime) override {
+    void onDraw(double deltaTime) override
+    {
         // Here, we just run a bunch of systems to control the world logic
         movementSystem.update(&world, (float)deltaTime);
-        cameraController.update(&world, (float)deltaTime);
+        cameraController.update(&world, (float)deltaTime, &counterToRemove, &numberOfBatteries);
+        counterToRemove++;
+
+        float speed = gameController.decreaseBatteries(&counterToRemove, &numberOfBatteries, false);
+        cameraController.changeSpeed(speed);
+        //    decreaseBatteries();
+
         // tempCount++;
         //   if(tempCount==400){
         //     tempCount=0;
         //     increaseBatteries();
         // }
         // And finally we use the renderer system to draw the scene
-        counterToRemove++;
-        decreaseBatteries();
 
-        //Collioison
-        int collisionState = collisionSystem.update(&world, (float)deltaTime);
-        if( collisionState == 1 )
-        {
-            increaseBatteries();
-        }
-        else if ( collisionState == -1 )
-        {
-            getApp()->changeState("gameover");
-        }
+        // decreaseBatteries();
+
+        // // Collioison
+        // int collisionState = collisionSystem.update(&world, (float)deltaTime);
+        // if (collisionState == 1)
+        // {
+        //     increaseBatteries();
+        // }
+        // else if (collisionState == -1)
+        // {
+        //     getApp()->changeState("gameover");
+        // }
 
         renderer.render(&world);
 
         // Get a reference to the keyboard object
-        auto& keyboard = getApp()->getKeyboard();
+        auto &keyboard = getApp()->getKeyboard();
 
-        if(keyboard.justPressed(GLFW_KEY_ESCAPE)){
+        if (keyboard.justPressed(GLFW_KEY_ESCAPE))
+        {
             // If the escape  key is pressed in this frame, go to the play state
             getApp()->changeState("menu");
         }
     }
 
-    void onDestroy() override {
+    void onDestroy() override
+    {
         // Don't forget to destroy the renderer
         renderer.destroy();
         // On exit, we call exit for the camera controller system to make sure that the mouse is unlocked
